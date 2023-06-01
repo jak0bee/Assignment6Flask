ebe1ca074c224f56320f5e86efef4bb631459651
jak0bee <115256446+jak0bee@users.noreply.github.com>
2023-06-01 14:29:23 +0200

    book_inventory put

--- book_catalog.py
+++ book_catalog.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, abort, request
+import requests
 
 app = Flask(__name__)
 
--- book_inventory.py
+++ book_inventory.py
@@ -1,4 +1,4 @@
-from flask import Flask, abort, json, request
+from flask import Flask, abort, request, jsonify
 
 app = Flask(__name__)
 
@@ -7,3 +7,14 @@ inventory = [
     {"book_id": 2, "quantity": 3}
 ]
 
+
+@app.route('/inventory/add', methods=['PUT'])
+def add_book():
+    if not request.json or 'book_id' not in request.json:
+        abort(404)
+    new_book = {
+        "book_id": request.json.get['book_id'],
+        "quantity": 2
+    }
+    inventory.append(new_book)
+    return jsonify({'book': new_book}), 201
